5e6bab5fa16392d975a4af542f090fb6d201f1cf
segerberg <andreassegerberg@gmail.com>
2022-03-31 14:53:11 +0200

    Take sysarg

--- install_default_bots.py
+++ install_default_bots.py
@@ -1,19 +1,24 @@
 import csv
+import sys
 from app import app, db
 from app.models import Bot
 from sqlalchemy.exc import IntegrityError
 
-with open('bot_names.csv', 'r', encoding='utf-8') as f:
-    reader = csv.reader(f, delimiter=',')
-    next(reader)
+def main(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter=',')
+        next(reader)
 
-    for name, year_of_birth, sex in reader:
-        try:
-            bot = Bot(name=name, year_of_birth=year_of_birth, sex=sex, default=True)
-            db.session.add(bot)
-            db.session.commit()
-        except IntegrityError:
-            print(f'{name} already exists')
-            db.session.rollback()
-            continue
+        for name, year_of_birth, sex in reader:
+            try:
+                bot = Bot(name=name, year_of_birth=year_of_birth, sex=sex, default=True)
+                db.session.add(bot)
+                db.session.commit()
+            except IntegrityError:
+                print(f'{name} already exists')
+                db.session.rollback()
+                continue
 
+if __name__ == '__main__':
+    file = sys.argv[1]
+    main(file)
